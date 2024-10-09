de23407f451b58d1f7064eb6bcc7a2739098b5e6
Sauciuc Matei <matei@goosfraba.ro>
2024-10-09 17:35:58 +0300

    primul seminar

---
+++ test.js
@@ -0,0 +1,16 @@
+function test () {
+    if(true){
+        var var1 = 1;
+        let let2 =2;
+        const const3 = 3;
+    }
+    console.log(var1)
+    console.log(let2)
+    console.log(const3);
+}
+// test();
+// obj.name = 'Adrian'
+let obj = {}
+
+const arr = [1,2,3,4]
+console.log(arr)
